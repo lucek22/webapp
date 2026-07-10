dcf56b1b2399fb082a9ca41b2cb88f8fdc767b6a
kalebl <kalebl@bgsu.edu>
2026-07-10 11:13:47 -0400

    Fix small error

--- userController.js
+++ userController.js
@@ -342,6 +342,20 @@ export function drawFullSkeletalMesh(landmarks, ctx = canvasCtx) {
   });
 }
 
+function drawRoundedRect(ctx, x, y, width, height, radius) {
+  ctx.beginPath();
+  if (ctx.roundRect) {
+    ctx.roundRect(x, y, width, height, radius);
+  } else {
+    ctx.moveTo(x + radius, y);
+    ctx.arcTo(x + width, y, x + width, y + height, radius);
+    ctx.arcTo(x + width, y + height, x, y + height, radius);
+    ctx.arcTo(x, y + height, x, y, radius);
+    ctx.arcTo(x, y, x + width, y, radius);
+  }
+  ctx.closePath();
+}
+
 function drawAngleBadge(ctx, point, value, color) {
   if (!point || value === undefined || value === null || isNaN(value)) return;
 
